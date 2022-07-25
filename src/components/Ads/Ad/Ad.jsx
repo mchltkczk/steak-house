193d67a1820544b14ba874f46036556e65b0334c
Michal Tkaczuk <mchltkczk@gmail.com>
2022-07-26 01:23:03 +0200

    Ads background renders based on props

--- src/components/Ads/Ad/Ad.jsx
+++ src/components/Ads/Ad/Ad.jsx
@@ -1,3 +1,11 @@
-export const Ad = () => {
-  return <div style={{ backgroundColor: "black", width: "100%" }}>s</div>;
+import styles from "./Ad.module.scss";
+
+export const Ad = ({ header, text, background }) => {
+  return (
+    <div className={`${styles.wrapper} ${styles[background]}`}>
+      <h3>{header}</h3>
+      <p>{text}</p>
+      <div>Przycisk</div>
+    </div>
+  );
 };
--- src/components/Ads/Ads.jsx
+++ src/components/Ads/Ads.jsx
@@ -1,10 +1,20 @@
 import { Ad } from "./Ad/Ad";
 import styles from "./Ads.module.scss";
+import plates from "../../assets/plates.png";
 export const Ads = () => {
   return (
     <div className={styles.wrapper}>
-      <Ad />
-      <Ad />
+      <Ad
+        header='Zestaw dla 3 osób'
+        text='Razem smakuje lepiej! Zamów zestawy obiadowy dla 3 osób 
+już od 39 zł '
+        background='left'
+      />
+      <Ad
+        header='UberEats'
+        text='Zamawiaj w aplikacji 10% taniej.
+Pobierz aplikację UberEats już dziś.'
+      />
     </div>
   );
 };
